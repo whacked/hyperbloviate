08ca509473ac7feb53cae7f41dbfd026aa528e75
natto <natto@natto-tpy.none>
2017-01-22 13:33:07 -0800

    add support for loading relative path preamble/postamble file

--- client.el
+++ client.el
@@ -3,6 +3,10 @@
 (setq drivable/jrpc-port 8002)
 (setf drivable/jrpc-handle (json-rpc-connect "localhost" drivable/jrpc-port))
 (setq drivable/jrpc-endpoint "/rpc")
+
+(setq drivable/sibilant-preamble-file "macro.sibilant")
+(setq drivable/sibilant-postamble-file nil)
+
 (defun drivable/json-rpc-request (connection method &rest mixed-params)
   "Send request of METHOD to CONNECTION, returning result or signalling error."
   (let* ((id (cl-incf (json-rpc-id-counter connection)))
@@ -31,10 +35,22 @@
 (defun drivable/eval-sibilant (sib)
   (drivable/json-rpc-request drivable/jrpc-handle "sibilant" (list sib)))
 
+(defun drivable/maybe-read-file (filepath)
+  (if (and filepath
+           (file-exists-p filepath))
+      (with-temp-buffer
+        (insert-file-contents filepath)
+        (buffer-string))
+    ""))
+
 ;; include sibilant-skewer-mode first
 (defun sibilant-jsonrpc-send-region (beg end)
   (let ((selected (buffer-substring beg end)))
-    (drivable/eval-sibilant selected)))
+    (drivable/eval-sibilant
+     (concat
+      (drivable/maybe-read-file drivable/sibilant-preamble-file)
+      selected
+      (drivable/maybe-read-file drivable/sibilant-postamble-file)))))
 
 (defun sibilant-jsonrpc-eval-preceding-sexp (&optional prefix)
   (interactive "P")
