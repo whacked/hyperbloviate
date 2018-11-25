e281e3be8ac5f26df2534dc2b45c607eb6aad110
natto <whacked@users.noreply.github.com>
2018-11-25 11:04:10 -0800

    clean up preload imports

--- electron-preload.js
+++ electron-preload.js
@@ -1,31 +1,26 @@
 console.log("preloader");
 
 const electron = require('electron');
-const shell = electron.shell;
-const ipc = electron.ipcRenderer;
 
 window.addEventListener('load', ()  => {
   // required for golden layout to function
   window.$ = require("./node_modules/jquery/dist/jquery.min.js");
 
-  const common = require("./src/common"),
-        Const = common.Const,
-        Immutable = require("./node_modules/immutable/dist/immutable.min.js"),
+  const Immutable = require("./node_modules/immutable/dist/immutable.min.js"),
         m = require("./node_modules/mithril/mithril.min.js"),
         sibilant = require("./node_modules/sibilant/lib/browser.js").sibilant,
         Hyperbloviate = require("./src/hyperbloviate.js"),
         JsonRpcServer = require("./src/jsonRpcServer.js").Server,
-        {ipcRenderer} = require("electron");
+        WebviewControl = require("./src/webview");
 
   JsonRpcServer.start();
 
   Hyperbloviate.setupDefaultUi(document.body);
   
   window.Hyperbloviate = Hyperbloviate;
-  window.ipcRenderer = ipcRenderer;
+  window.ipcRenderer = electron.ipcRenderer;
 
   // for plugin interaction
-  const WebviewControl = require("./src/webview");
   window.webviewControl = WebviewControl;
   window.execJs = WebviewControl.execJs;
   window.getWebview = WebviewControl.getWebview;
