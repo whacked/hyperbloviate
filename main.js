065b0001a4efeae480f2d33889881a59f34fa162
natto <whacked@users.noreply.github.com>
2018-11-26 23:03:59 -0800

    Update package def and entrypoint for package import use

--- main.js
+++ main.js
@@ -1,29 +1,36 @@
-const  {app, BrowserWindow, ipcMain} = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 
-let mainWindow;
+if (app) {
+    let mainWindow;
 
-app.on("window-all-closed", function() {
-    app.quit();
-});
+    app.on("window-all-closed", function () {
+        app.quit();
+    });
 
-app.on("ready", function() {
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-            preload: path.resolve(path.join(__dirname, "electron-preload.js"))
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
-    mainWindow.openDevTools();
+    app.on("ready", function () {
+        mainWindow = new BrowserWindow({
+            webPreferences: {
+                preload: path.resolve(path.join(__dirname, "electron-preload.js"))
+            }
+        });
+        mainWindow.loadURL(`file://${__dirname}/index.html`);
+        mainWindow.openDevTools();
 
-    ipcMain.on("ipc", (event, arg) => {
-        console.log("---- got event in ipc");
-        console.log(event);
-        console.log(arg);
+        ipcMain.on("ipc", (event, arg) => {
+            console.log("---- got event in ipc");
+            console.log(event);
+            console.log(arg);
+        });
+        ipcMain.on("test", (event, arg) => {
+            console.log("---- got event in TEST");
+            console.log(event);
+            console.log(arg);
+        });
     });
-    ipcMain.on("test", (event, arg) => {
-        console.log("---- got event in TEST");
-        console.log(event);
-        console.log(arg);
-    });
-});
+} else {
+    // export namespace for package use
+    exports.Hyperbloviate = require("./src/hyperbloviate");
+    exports.JsonRpcServer = require("./src/jsonRpcServer.js");
+    exports.WebviewControl = require("./src/webview");
+}
